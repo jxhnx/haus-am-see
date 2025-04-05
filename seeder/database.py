c949836fd066d0131655e9e4dec8661cfea89031
John Hoffmann <johnvonhoffmann@gmail.com>
2025-04-05 10:07:58 +0200

    fix(source): env vars

--- seeder/database.py
+++ seeder/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 STOREFRONT_DB_URL = os.getenv(
-    "DATABASE_URL", "postgresql+psycopg://postgres:postgres@postgres:5432/storefront"
+    "DATABASE_URL",
+    "postgresql+psycopg://postgres:postgres@postgres_source:5432/storefront",
 )
 engine = create_engine(STOREFRONT_DB_URL)
 SessionLocal = sessionmaker(bind=engine)
